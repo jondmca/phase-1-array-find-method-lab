c29fc5ed338668d53c519d412d24ba17f8eeef7f
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-30 20:15:05 -0500

    commit tree 2f95d0433cecc57a098316f428cc9ba6970859ed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    let found = record.find(object => object.result === "W")
+    if (found == null){
+        return found
+    }
+    else {
+        return found.year 
+}
+}
